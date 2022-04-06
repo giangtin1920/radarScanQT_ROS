12e97a6f763a424191c3a38cd22e644371800fa9
giangtin1920 <giangtin1920@gmail.com>
2022-04-06 22:56:05 +0700

    git ignorg update

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/giangtin/aev/radarScanQT_ROS/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/giangtin/aev/radarScanQT_ROS/devel;/home/giangtin/aev/simulation_ws/devel;/home/giangtin/Downloads/obstacleAvoidanceRobot/catkin_ws/devel;/home/giangtin/aev/aev_ws/devel;/home/giangtin/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/giangtin/aev/radarScanQT_ROS/devel;/home/giangtin/aev/simulation_ws/devel;/home/giangtin/Downloads/obstacleAvoidanceRobot/catkin_ws/devel;/home/giangtin/aev/aev_ws/devel;/home/giangtin/catkin_ws/devel;/opt/ros/melodic' != '' else []
+underlay_workspaces = '/home/giangtin/aev/simulation_ws/devel;/home/giangtin/Downloads/obstacleAvoidanceRobot/catkin_ws/devel;/home/giangtin/aev/aev_ws/devel;/home/giangtin/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/giangtin/aev/simulation_ws/devel;/home/giangtin/Downloads/obstacleAvoidanceRobot/catkin_ws/devel;/home/giangtin/aev/aev_ws/devel;/home/giangtin/catkin_ws/devel;/opt/ros/melodic' != '' else []
